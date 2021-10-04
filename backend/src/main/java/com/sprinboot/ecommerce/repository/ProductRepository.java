3a21e6c44907b493e9135b5862e1cb26cec5aafe
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-04 14:56:22 +0200

    Task 17: working with search for products by keyword (backend)

--- backend/src/main/java/com/sprinboot/ecommerce/repository/ProductRepository.java
+++ backend/src/main/java/com/sprinboot/ecommerce/repository/ProductRepository.java
@@ -18,4 +18,5 @@ import org.springframework.web.bind.annotation.RequestParam;
 @CrossOrigin("http://localhost:4200/")
 public interface ProductRepository extends JpaRepository<Product,Long> {
     Page<Product> findByCategoryId(@RequestParam("id")Long id, Pageable  pageable);
+    Page<Product> findByNameContainingIgnoreCase(@RequestParam("name") String name,Pageable pageable);
 }
