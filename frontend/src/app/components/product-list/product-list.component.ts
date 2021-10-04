44b39494f9d52dd8e36039cd5b326dcb1bd4244e
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-04 13:32:22 +0200

    Task 14: search for product by category code (enhance productlist)

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -1,35 +1,39 @@
 import { ProductService } from './../../services/product.service';
 import { Product } from './../../common/product';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
+  products: Product[];
+  currentCategoryId: number;
 
-  products:Product[];
-  allowServers=false;
-
-  constructor(private _productService:ProductService) { }
+  constructor(
+    private _productService: ProductService,
+    private _activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.onProductList();
-    console.log("allowServers",this.allowServers);
-    setTimeout(() => {
-      this.allowServers=true;
-    }, 5000);
+    this._activatedRoute.paramMap.subscribe(() => {
+      this.onProductList();
+    });
+  }
+
+  onProductList() {
+    const hasCategoryId=this._activatedRoute.snapshot.paramMap.has('id');
+    if(hasCategoryId){
+      this.currentCategoryId=+this._activatedRoute.snapshot.paramMap.get('id');
+    }else{
+      this.currentCategoryId=1;
+    }
+
+    this._productService.getProductListByCategoryId(this.currentCategoryId).subscribe((response) => {
+      this.products = response;
+    });
     
   }
-
-  onProductList(){
-    this._productService.getProductList().subscribe(
-      (response)=>{
-        
-        this.products=response;
-      }
-    )
-  }
-
 }
