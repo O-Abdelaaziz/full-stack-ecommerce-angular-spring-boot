1c0cc5b133f5cd6ef9c1e64fde7ea8617ac033e6
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-06 13:59:29 +0200

    Task 26: create & develop cart-service

---
+++ frontend/src/app/services/cart.service.ts
@@ -0,0 +1,53 @@
+import { CartItem } from './../common/cart-item';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems:CartItem[]=[];
+
+  totalPrice:Subject<number> =new Subject<number>();
+  totalQuantity:Subject<number> =new Subject<number>();
+  
+  constructor() { }
+
+  addToCart(cartItem:CartItem){
+    //check if we have already the item in our cart
+    let alreadyExistInCart:boolean=false;
+    let existingCartItem:CartItem=undefined;
+
+    if(this.cartItems.length>0){
+      for(let tempCartItem of this.cartItems){
+        if(tempCartItem.id === cartItem.id){
+          existingCartItem=tempCartItem;
+          break;
+        }
+
+        alreadyExistInCart= (existingCartItem != undefined);
+
+        if(alreadyExistInCart){
+          existingCartItem.quantity ++;
+        }else{
+          this.cartItems.push(cartItem)
+        }
+
+        this.computeCartTotals();
+      }
+    }
+  }
+  computeCartTotals() {
+    let totalPriceValue=0
+    let totalQuantityValue=0
+
+    for(let cartItem of this.cartItems){
+      totalPriceValue += cartItem.quantity * cartItem.unitPrice;
+      totalQuantityValue += cartItem.quantity;
+    }
+
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+  }
+}
