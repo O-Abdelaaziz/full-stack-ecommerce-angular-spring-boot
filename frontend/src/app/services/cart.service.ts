1cee19cab460c56cce5b96d0a378a69e2b9a2ad2
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-06 15:52:11 +0200

    Task 30: working with increment & decrement & remove item from cartstatus & cartdetails

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -6,6 +6,7 @@ import { Subject } from 'rxjs';
   providedIn: 'root',
 })
 export class CartService {
+
   cartItems: CartItem[] = [];
 
   totalPrice: Subject<number> = new Subject<number>();
@@ -49,4 +50,25 @@ export class CartService {
     this.totalPrice.next(totalPriceValue);
     this.totalQuantity.next(totalQuantityValue);
   }
+
+  decrementQuantity(cartItem: CartItem) {
+    cartItem.quantity--;
+
+    if(cartItem.quantity ===0){
+      this.remove(cartItem);
+    }else{
+      this.computeCartTotals();
+    }
+  }
+
+
+  remove(cartItem: CartItem) {
+    const itemIndex=this.cartItems.findIndex(cartItemTemp=> cartItemTemp.id===cartItem.id);
+    if(itemIndex>-1){
+      this.cartItems.splice(itemIndex,1);
+      this.computeCartTotals();
+    }
+  }
+
+  
 }
