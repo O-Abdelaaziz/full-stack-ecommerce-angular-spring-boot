e3ffb08f31a57f231d75b81472e928631d30532f
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-10 14:54:07 +0200

    Task 39: working with checkout form layout & review your order

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -4,57 +4,58 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
+  checkoutFormGroup: FormGroup;
+  totalQuantity: number = 0;
+  totalPrice: number = 0;
 
-  checkoutFormGroup :FormGroup;
-
-  constructor(private _formBuilder:FormBuilder) { }
+  constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.checkoutFormGroup=this._formBuilder.group({
-      customer:this._formBuilder.group({
-        firstName:[''],
-        lastName:[''],
-        email:[''],
+    this.checkoutFormGroup = this._formBuilder.group({
+      customer: this._formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: [''],
       }),
-      shippingAddress:this._formBuilder.group({
-        street:[''],
-        city:[''],
-        state:[''],
-        country:[''],
-        zipCode:[''],
+      shippingAddress: this._formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
       }),
       billingAddress: this._formBuilder.group({
         street: [''],
         city: [''],
         state: [''],
         country: [''],
-        zipCode: ['']
+        zipCode: [''],
       }),
-      creditCard:this._formBuilder.group({
-        cardType:[''],
-        nameOnCard:[''],
-        cardNumber:[''],
-        securityCode:[''],
-        expirationMonth:[''],
-        expirationYear:[''],
-      })
-    })
+      creditCard: this._formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: [''],
+      }),
+    });
   }
 
-  copyShippingAddressToBillingAddress(event){
-    if(event.target.checked){
-      this.checkoutFormGroup.controls.billingAddress.setValue(this.checkoutFormGroup.controls.shippingAddress.value);
-    }else{
+  copyShippingAddressToBillingAddress(event) {
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls.billingAddress.setValue(
+        this.checkoutFormGroup.controls.shippingAddress.value
+      );
+    } else {
       this.checkoutFormGroup.controls.billingAddress.reset();
     }
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.checkoutFormGroup.get('customer').value);
-    
   }
-
 }
