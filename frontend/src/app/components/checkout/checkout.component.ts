b00bf0caf0400a64d85d9943dde12cc9b0c02964
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-10 14:21:32 +0200

    Task 32: working with checkout form layout & add support for reactive forms

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  checkoutFormGroup :FormGroup;
+
+  constructor(private _formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.checkoutFormGroup=this._formBuilder.group({
+      costumer:this._formBuilder.group({
+        firstName:[''],
+        lastName:[''],
+        email:[''],
+      })
+    })
   }
 
 }
