e7eb7e9ef991a17c20d711f1511a2af9ed000ffd
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-11 13:47:03 +0200

    Task 45: working with checkout form & populate countrie and state repositories

--- backend/src/main/java/com/sprinboot/ecommerce/repository/StateRepository.java
+++ backend/src/main/java/com/sprinboot/ecommerce/repository/StateRepository.java
@@ -1,18 +1,16 @@
 package com.sprinboot.ecommerce.repository;
 
-import com.sprinboot.ecommerce.entity.ProductCategory;
+import com.sprinboot.ecommerce.entity.State;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
- * @Created 03/10/2021 - 13:48
+ * @Created 11/10/2021 - 13:41
  * @Package com.sprinboot.ecommerce.repository
  * @Project sprin-boot-ecommerce
  * @User LegendDZ
  * @Author Abdelaaziz Ouakala
  **/
-@RepositoryRestResource(collectionResourceRel = "productCategory",path = "product-category")
 @CrossOrigin("http://localhost:4200/")
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
+public interface StateRepository extends JpaRepository<State,Long> {
 }
