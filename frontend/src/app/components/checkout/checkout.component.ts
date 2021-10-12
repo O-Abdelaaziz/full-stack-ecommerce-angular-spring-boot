6fb85937507ed1ebc4d228cf799b0b8c3ee733dc
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-12 14:10:58 +0200

    Task 55: working with checkout form & billing validation

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -41,11 +41,11 @@ export class CheckoutComponent implements OnInit {
         zipCode: new FormControl('',[Validators.required,Validators.minLength(2),CustomValidators.notOnlyWhiteSpace]),
       }),
       billingAddress: this._formBuilder.group({
-        street: [''],
-        city: [''],
-        state: [''],
-        country: [''],
-        zipCode: [''],
+        street: new FormControl('',[Validators.required,Validators.minLength(2),CustomValidators.notOnlyWhiteSpace]),
+        city: new FormControl('',[Validators.required,Validators.minLength(2),CustomValidators.notOnlyWhiteSpace]),
+        state: new FormControl('',[Validators.required,Validators.minLength(2),CustomValidators.notOnlyWhiteSpace]),
+        country: new FormControl('',[Validators.required,Validators.minLength(2),CustomValidators.notOnlyWhiteSpace]),
+        zipCode: new FormControl('',[Validators.required,Validators.minLength(2),CustomValidators.notOnlyWhiteSpace]),
       }),
       creditCard: this._formBuilder.group({
         cardType: [''],
@@ -85,6 +85,12 @@ export class CheckoutComponent implements OnInit {
   get shippingAddressState(){return this.checkoutFormGroup.get('shippingAddress.state');}
   get shippingAddressCountry(){return this.checkoutFormGroup.get('shippingAddress.country');}
   get shippingAddressZipCode(){return this.checkoutFormGroup.get('shippingAddress.zipCode');}
+
+  get billingAddressStreet(){return this.checkoutFormGroup.get('billingAddress.street');}
+  get billingAddressCity(){return this.checkoutFormGroup.get('billingAddress.city');}
+  get billingAddressState(){return this.checkoutFormGroup.get('billingAddress.state');}
+  get billingAddressCountry(){return this.checkoutFormGroup.get('billingAddress.country');}
+  get billingAddressZipCode(){return this.checkoutFormGroup.get('billingAddress.zipCode');}
 
   onSubmit() {
     console.log(this.checkoutFormGroup.get('customer').value);
