486f4330d20abdb8b5547cd880bf5cf3173019be
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-17 12:02:25 +0200

    Task 58: working with checkout form & save the order to database

---
+++ backend/src/main/java/com/sprinboot/ecommerce/entity/OrderItem.java
@@ -0,0 +1,47 @@
+package com.sprinboot.ecommerce.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+/**
+ * @Created 13/10/2021 - 13:10
+ * @Package com.sprinboot.ecommerce.entity
+ * @Project sprin-boot-ecommerce
+ * @User LegendDZ
+ * @Author Abdelaaziz Ouakala
+ **/
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "imageUrl")
+    private String imageUrl;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "quantity")
+    private String quantity;
+
+    @Column(name = "unit_price")
+    private String unit_price;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+
+}
