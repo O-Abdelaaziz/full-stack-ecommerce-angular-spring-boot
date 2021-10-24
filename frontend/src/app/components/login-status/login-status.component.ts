d7b4772aa9a20319822bfcb726c2d62957104d0d
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-24 14:39:11 +0200

    Task 69: working with okta security (create & add css for login status component)

---
+++ frontend/src/app/components/login-status/login-status.component.ts
@@ -0,0 +1,40 @@
+import { OktaAuthService } from '@okta/okta-angular';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-status',
+  templateUrl: './login-status.component.html',
+  styleUrls: ['./login-status.component.css']
+})
+export class LoginStatusComponent implements OnInit {
+  isAuthenticated:boolean=false;
+  userFullName:string='';
+  constructor(private oktaAuthService:OktaAuthService) { }
+
+  ngOnInit(): void {
+    this.oktaAuthService.$authenticationState.subscribe(
+      (response)=>{
+        this.isAuthenticated=response;
+        this.getUserDetails();
+      }
+    )
+  }
+
+
+  getUserDetails() {
+
+
+    if(this.isAuthenticated){
+      this.oktaAuthService.getUser().then(
+        (response)=>{
+          this.userFullName=response.name;
+        }
+      )
+    }
+  }
+
+  logout(){
+    this.oktaAuthService.signOut();
+  }
+
+}
