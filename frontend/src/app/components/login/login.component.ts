16db502291e63ed92ae8dc788e2f49a325fd686f
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-26 08:48:10 +0200

    Task 78: working with order history (secure backend)

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 import { OktaAuthService } from '@okta/okta-angular';
-import { OktaAuth, IDToken, AccessToken } from '@okta/okta-auth-js';
-
 import * as OktaSignIn from '@okta/okta-signin-widget';
 import appConfig from 'src/app/config/app-config';
 @Component({
@@ -12,41 +10,41 @@ import appConfig from 'src/app/config/app-config';
 })
 export class LoginComponent implements OnInit {
 
-  oktaSignIn:any;
-  constructor(private oktaAuthService:OktaAuthService) { 
-    this.oktaSignIn=new OktaSignIn(
-      {
-        logo:'assets/images/logo.png',
-        features:{
-          registration:true
-        },
-        baseUrl:appConfig.oidc.issuer.split('/oauth2')[0],
-        clientId:appConfig.oidc.clientId,
-        redirectUri:appConfig.oidc.redirectUri,
-        authParams:{
-          pkce:true,
-          issuer:appConfig.oidc.issuer,
-          scopes:appConfig.oidc.scopes
-        }
+  oktaSignin: any;
+
+  constructor(private oktaAuthService: OktaAuthService) { 
+
+    this.oktaSignin = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      features: {
+        registration: true
+      },
+      baseUrl: appConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: appConfig.oidc.clientId,
+      redirectUri: appConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: appConfig.oidc.issuer,
+        scopes: appConfig.oidc.scopes
       }
-    );
+    });
 
   }
 
-  
-
   ngOnInit(): void {
-    this.oktaSignIn.remove();
-    this.oktaSignIn.renderEl({
-      el:'#okta-sign-in-widget'},
-      (response)=>{
-        if(response.status ==='SUCCESS'){
+    this.oktaSignin.remove();
+
+    this.oktaSignin.renderEl({
+      el: '#okta-sign-in-widget'}, // this name should be same as div tag id in login.component.html
+      (response) => {
+        if (response.status === 'SUCCESS') {
           this.oktaAuthService.signInWithRedirect();
         }
-       },
-       (error)=>{
+      },
+      (error) => {
         throw error;
-      });
+      }
+    );
   }
 
 }
