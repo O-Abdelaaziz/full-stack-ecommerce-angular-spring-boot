7691545a8daec4f0f060d743417758da8048fd7b
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-26 11:17:53 +0200

    Task 81: secure communication with https (angular frontend)

--- frontend/src/app/config/app-config.ts
+++ frontend/src/app/config/app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oa2cvc8blmOJg1jK5d7',
     issuer: 'https://dev-22214668.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes:['openid','profile','email']
   }
 };
